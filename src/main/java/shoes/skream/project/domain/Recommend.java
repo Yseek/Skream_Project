a8eb79b10eb967cd8af8d6876ec146f97bf6d681
gorilla2005 <gorilla2005@naver.com>
2023-03-23 12:37:01 +0900

    a

--- src/main/java/shoes/skream/project/domain/Recommend.java
+++ src/main/java/shoes/skream/project/domain/Recommend.java
@@ -1,9 +1,11 @@
 package shoes.skream.project.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
