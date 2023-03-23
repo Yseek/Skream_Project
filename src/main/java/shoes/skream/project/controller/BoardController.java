7f145d097d3be3dd14114d44c502c404b3063dcc
YunSeek Jeon <yun5003@naver.com>
2023-03-23 10:53:10 +0900

    게시판, 상세페이지 초기

---
+++ src/main/java/shoes/skream/project/controller/BoardController.java
@@ -0,0 +1,12 @@
+package shoes.skream.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	@GetMapping("content")
+	public String content(){
+		return "content";
+	}
+}
