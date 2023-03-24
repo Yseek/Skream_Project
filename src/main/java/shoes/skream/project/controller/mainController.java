f10735daac59d8860e4cc12464700893e0d50cb9
gorilla2005 <gorilla2005@naver.com>
2023-03-24 12:29:30 +0900

    머어지

---
+++ src/main/java/shoes/skream/project/controller/mainController.java
@@ -0,0 +1,12 @@
+package shoes.skream.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+  @GetMapping("main")
+	public String content(){
+		return "main";
+	} 
+}
