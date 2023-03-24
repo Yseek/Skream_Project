ad50d6ea7358d06deb2107882faad243f38ce55d
rjadmlantk9-test <rjadmlantk9@naver.com>
2023-03-24 16:29:18 +0900

    대원형 커밋

---
+++ src/main/java/shoes/skream/project/controller/hyun/MainController.java
@@ -0,0 +1,12 @@
+package shoes.skream.project.controller.hyun;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("main")
+    public String main() {
+        return "main";
+    }
+}
