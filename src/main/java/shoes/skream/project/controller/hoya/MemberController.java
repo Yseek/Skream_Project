aea71e88d3cd42c0e330c8a8c0c6f94edcf63e6d
gorilla2005 <gorilla2005@naver.com>
2023-03-27 19:16:42 +0900

    aa

--- src/main/java/shoes/skream/project/controller/hoya/MemberController.java
+++ src/main/java/shoes/skream/project/controller/hoya/MemberController.java
@@ -3,7 +3,6 @@ package shoes.skream.project.controller.hoya;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
