3da101db54b6b474d6519ddd4b300477d694b685
dangeeeeeeeeeee <dangeeeeeeeeeee@gmail.com>
2023-03-27 19:31:20 +0900

    모두 합치기 전

--- src/main/java/shoes/skream/project/domain/Boardfile.java
+++ src/main/java/shoes/skream/project/domain/Boardfile.java
@@ -2,13 +2,9 @@ package shoes.skream.project.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,10 +23,4 @@ public class Boardfile {
 	private long fileupFileId;
 	@Column(name = "board_id")
 	private long boardId;
-/* 	@ManyToOne
-	@JoinColumn(name = "fileup_file_id")
-	private Fileup fileup;
-	@ManyToOne
-	@JoinColumn(name = "board_id")
-	private Board board; */
 }
