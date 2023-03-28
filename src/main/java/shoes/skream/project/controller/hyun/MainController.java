9eecc9ade479d176c6fe15ca77a6c5be40ad2469
YunSeek Jeon <yun5003@naver.com>
2023-03-28 17:33:08 +0900

    대원이형

--- src/main/java/shoes/skream/project/controller/hyun/MainController.java
+++ src/main/java/shoes/skream/project/controller/hyun/MainController.java
@@ -26,62 +26,26 @@ public class MainController {
 		if (orderby.equals("hits")) {
 			Page<BoardDto> list = boardServiceInterface.listByView(pageable);
 			int nowPage = list.getPageable().getPageNumber() + 1;
-			int startPage = Math.max(nowPage - 4, 1);
-			int endPage = Math.min(nowPage + 4, list.getTotalPages());
-			if (endPage < 10) {
-				endPage = Math.min(10, list.getTotalPages());
-			}
-			if (endPage - startPage < 10) {
-				startPage = Math.max(endPage - 9, 1);
-			}
-			int size = list.getPageable().getPageSize();
+
 			model.addAttribute("orderby", orderby);
 			model.addAttribute("list", list);
 			model.addAttribute("nowPage", nowPage);
-			model.addAttribute("startPage", startPage);
-			model.addAttribute("endPage", endPage);
-			model.addAttribute("size", size);
-			model.addAttribute("total", list.getTotalPages());
 			return "main";
 		} else if (orderby.equals("recom")) {
 			Page<BoardDto> list = boardServiceInterface.listByRecom(pageable);
 			int nowPage = list.getPageable().getPageNumber() + 1;
-			int startPage = Math.max(nowPage - 4, 1);
-			int endPage = Math.min(nowPage + 4, list.getTotalPages());
-			if (endPage < 10) {
-				endPage = Math.min(10, list.getTotalPages());
-			}
-			if (endPage - startPage < 10) {
-				startPage = Math.max(endPage - 9, 1);
-			}
-			int size = list.getPageable().getPageSize();
+
 			model.addAttribute("orderby", orderby);
 			model.addAttribute("list", list);
 			model.addAttribute("nowPage", nowPage);
-			model.addAttribute("startPage", startPage);
-			model.addAttribute("endPage", endPage);
-			model.addAttribute("size", size);
-			model.addAttribute("total", list.getTotalPages());
 			return "main";
 		} else {
 			Page<BoardDto> list = boardServiceInterface.list(pageable);
 			int nowPage = list.getPageable().getPageNumber() + 1;
-			int startPage = Math.max(nowPage - 4, 1);
-			int endPage = Math.min(nowPage + 4, list.getTotalPages());
-			if (endPage < 10) {
-				endPage = Math.min(10, list.getTotalPages());
-			}
-			if (endPage - startPage < 10) {
-				startPage = Math.max(endPage - 9, 1);
-			}
-			int size = list.getPageable().getPageSize();
+
 			model.addAttribute("orderby", orderby);
 			model.addAttribute("list", list);
 			model.addAttribute("nowPage", nowPage);
-			model.addAttribute("startPage", startPage);
-			model.addAttribute("endPage", endPage);
-			model.addAttribute("size", size);
-			model.addAttribute("total", list.getTotalPages());
 			return "main";
 		}
 	}
