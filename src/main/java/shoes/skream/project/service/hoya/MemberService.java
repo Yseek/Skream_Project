8a5d14a6a225d543522a04515af5591c086f2a1c
YunSeek Jeon <yun5003@naver.com>
2023-03-29 12:02:33 +0900

    컨트롤러 오류 수정

--- src/main/java/shoes/skream/project/service/hoya/MemberService.java
+++ src/main/java/shoes/skream/project/service/hoya/MemberService.java
@@ -16,47 +16,49 @@ import shoes.skream.project.repository.hoya.MemberRepository;
 @RequiredArgsConstructor
 public class MemberService {
 
-  private  final MemberRepository memberRepository;
-  
-  private  final MemberDtoMapper memberDtoMapper;
+	private final MemberRepository memberRepository;
 
-  public void save(MemberDTO memberDTO) {
-      Member member = Member.toMember(memberDTO);
-      memberRepository.save(member);
-  }
-   
-  public MemberDTO login(MemberDTO memberDTO) {
-      Optional<Member> byEmail =memberRepository.findByEmail(memberDTO.getEmail());
-      if(byEmail.isPresent()) {
-        Member member = byEmail.get();
-        if(member.getPwd().equals(memberDTO.getPwd())){
-          MemberDTO dto=MemberDTO.toMemberDTO(member);
-          return dto;
-        }else{
-          return null;
-        }
-      }else{
-        return null;
-      }
-  }
-  
-  /**
-   * 회원 정보 가져오기 
-   * @param memberDTO
-   * @return
-   */
- public MemberDTO member(String email) {
-	 	
-      return memberDtoMapper.member(email);
-  }
-  
-  /**
-   * 회원 정보 업데이트 
-   * @param memberDTO
-   * @return
-   */
- public int memberUpdatePost(MemberDTO memberDto) {
-	
-      return memberDtoMapper.memberUpdatePost(memberDto);
-  }
+	private final MemberDtoMapper memberDtoMapper;
+
+	public void save(MemberDTO memberDTO) {
+		Member member = Member.toMember(memberDTO);
+		memberRepository.save(member);
+	}
+
+	public MemberDTO login(MemberDTO memberDTO) {
+		Optional<Member> byEmail = memberRepository.findByEmail(memberDTO.getEmail());
+		if (byEmail.isPresent()) {
+			Member member = byEmail.get();
+			if (member.getPwd().equals(memberDTO.getPwd())) {
+				MemberDTO dto = MemberDTO.toMemberDTO(member);
+				return dto;
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * 회원 정보 가져오기
+	 * 
+	 * @param memberDTO
+	 * @return
+	 */
+	public MemberDTO member(String email) {
+
+		return memberDtoMapper.member(email);
+	}
+
+	/**
+	 * 회원 정보 업데이트
+	 * 
+	 * @param memberDTO
+	 * @return
+	 */
+	public int memberUpdatePost(MemberDTO memberDto) {
+
+		return memberDtoMapper.memberUpdatePost(memberDto);
+	}
 }
